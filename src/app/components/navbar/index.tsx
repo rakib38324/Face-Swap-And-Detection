6ac753b96efe23298afeb925d61e2b313f565bed
rakib38324 <rakib3832@gmail.com>
2024-07-01 01:12:44 +0600

    update code

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -64,9 +64,9 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <div className="navbar-end">
+        {/* <div className="navbar-end">
           <a className="btn">Button</a>
-        </div>
+        </div> */}
       </div>
     </section>
   );
